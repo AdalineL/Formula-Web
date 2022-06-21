571e63659ba0d4b4903e640305fec2c0f5f03024
Adaline <jiayinleong100@gmail.com>
2022-06-21 14:37:24 -0700

    added child processes to communicate with formula-dotnet

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -1,7 +1,36 @@
 import path from "path";
 import express from "express";
 
-const WebSocket = require("ws"); // new
+// const { spawn } = require("child_process");
+// const child_process = spawn("pwd");
+
+// var child = cp.fork("/src/index.js");
+
+//pseudo code for conserving number of child process created
+// numberOfChilds = 0;
+// if (numberOfChilds < socketServer.clients.size) {
+//   var numChildDiff = socketServer.clients.size - numberOfChilds;
+//   while (numChildDiff > 0) {
+//     fork();
+//   }
+// }
+
+//pseudo code for spawning a new child process for every client connection
+// socketServer.clients.forEach((client) => {
+//     fork();
+// });
+
+// child.on("exit", function (code, signal) {
+//   console.log(
+//     "child process exited with " + `code ${code} and signal ${signal}`
+//   );
+// });
+
+// var cp = require("child_process");
+const { spawn } = require("child_process");
+// const child_process = spawn("pwd");
+
+const WebSocket = require("ws");
 
 const app = express(),
   DIST_DIR = __dirname,
@@ -25,11 +54,42 @@ const messages = ["\n"];
 socketServer.on("connection", (socketClient) => {
   console.log("connected");
   console.log("Number of clients: ", socketServer.clients.size);
-  //console.log('username: ', socketServer.clients.size);
+
   socketClient.send(JSON.stringify(messages));
 
   socketClient.on("message", (message) => {
     console.log("message: ", message);
+
+    const child = spawn("dotnet", [
+      "formula-dotnet/Src/CommandLine/bin/Debug/net6.0/CommandLine.dll",
+    ]);
+
+    //'filepath to CommandLine.dll'
+
+    //fix so child process is continuous for each client connection
+    //so can continously update the terminal environment
+
+    //save the variables to 'tmp_file.txt'
+    var fs = require("fs");
+    var stream = fs.createWriteStream("tmp_file.4ml");
+    stream.once("open", function (fd) {
+      stream.write(message);
+      stream.end();
+    });
+
+    //send 'load file' to dotnet
+    child.stdin.write("load react-monaco-tree-sitter/tmp_file.4ml");
+    child.stdin.end();
+
+    //receive the data from dotnet and save it to a tmp file
+    child.stdout.on("data", (data) => {
+      var s = fs.createWriteStream("tmp_file_2.txt");
+      s.once("open", function (fd) {
+        s.write(data);
+        s.end();
+      });
+    });
+
     messages.push(message);
     socketServer.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
