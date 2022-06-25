92c0750281fda30045e35e217700d2aaad82e5cf
Adaline <jiayinleong100@gmail.com>
2022-06-24 17:58:24 -0700

    sent results from Formula-Dotnet to client side

--- src/js/TextEditor.js
+++ src/js/TextEditor.js
@@ -1,24 +1,6 @@
 import React, { useRef } from "react";
-import ReactDOM from "react-dom";
 
 import Editor, { useMonaco } from "@monaco-editor/react";
-import { useEffect } from "react";
-
-// const path = require("path");
-import "./patch-fetch.js";
-
-// import * as Monaco from "monaco-editor/esm/vs/editor/editor.api";
-//uncomment = parsing error from css minimizer
-//Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):
-//and ModuleParseError: Module parse failed: Unexpected character ''
-
-import * as Parser from "web-tree-sitter";
-import {
-  Language,
-  Theme,
-  MonacoTreeSitter,
-  highlight,
-} from "monaco-tree-sitter";
 
 function TextEditor() {
   const editorRef = useRef(null);
@@ -34,6 +16,7 @@ function TextEditor() {
 
     ws.onopen = () => {
       console.log("texteditor: Now connected");
+      //send monaco editor values to server
       ws.send(editorRef.current.getValue());
     };
   }
@@ -49,41 +32,6 @@ function TextEditor() {
                   s = sum(0.0, { c.utilization | 
                                  c is Component, Mapping(c, p) }), s > 100.
   }`;
-
-  // (async () => {
-  //   Theme.load(require("./theme.json")); //"monaco-tree-sitter/themes/tomorrow")); //to use the 'tomorrow' theme
-
-  //   await Parser.init();
-  //   const parser = new Parser();
-  //   const Lang = await Parser.Language.load("tree-sitter-formula.wasm");
-  //   parser.setLanguage(Lang);
-
-  //   const language = new Language(require("./grammar.json"));
-  //   await language.init(
-  //     "http://localhost:8080/tree-sitter-formula.wasm",
-  //     Parser
-  //   );
-  // })();
-
-  // (async () => {
-  //   const GSQL = await Parser.Language.load(
-  //     path.join(__dirname, "./tree-sitter-formula.wasm")
-  //   );
-  // })();
-
-  // const Parser = require("tree-sitter");
-  // const JavaScript = require("tree-sitter-javascript");
-
-  // const parser = new Parser();
-  // parser.setLanguage(JavaScript);
-
-  // const monaco = useMonaco();
-
-  // useEffect(() => {
-  //   if (monaco) {
-  //     console.log("here is the monaco instance:", monaco);
-  //   }
-  // }, [monaco]);
 
   return (
     <>
