a601a068b3fe49e6832fa99a300e7aa78757c47c
Adaline <90662211+AdalineL@users.noreply.github.com>
2022-07-15 09:15:19 -0700

    added tree-sitter-formula

--- src/js/EditorArea.js
+++ src/js/EditorArea.js
@@ -4,27 +4,20 @@
 
 import React, { useRef } from "react";
 import Editor, { useMonaco } from "@monaco-editor/react";
+import loader from "@monaco-editor/loader";
+import * as Parser from "web-tree-sitter";
+import {
+  Language,
+  Theme,
+  MonacoTreeSitter,
+  highlight,
+} from "monaco-tree-sitter";
 
 const { ws } = require("./configSocket.js"); //get the ws connection initialized in configSocket.js
 
 //the main functionality of the text-editor
 function EditorArea() {
   //refer to https://github.com/suren-atoyan/monaco-react for docs on @monaco-editor/react
-
-  const editorRef = useRef(null);
-
-  function handleEditorDidMount(editor, monaco) {
-    editorRef.current = editor;
-  }
-
-  //function to send text editor values to the server via WebSockets
-  document.getElementById("loadFile").onclick = function () {
-    var msg = {
-      type: "editor",
-      text: editorRef.current.getValue(),
-    };
-    ws.send(JSON.stringify(msg));
-  };
 
   //default code for the text editor
   const formulaCode = `
@@ -39,15 +32,52 @@ function EditorArea() {
                      c is Component, Mapping(c, p) }), s > 100.
   }`;
 
+  //load monaco editor into the browser
+  loader.init().then((monaco) => {
+    loadTreeSitter();
+    const wrapper = document.getElementById("textEditor");
+    wrapper.style.height = "100%";
+    const properties = {
+      value: formulaCode,
+      language: "javascript",
+    };
+
+    window.editor = monaco.editor.create(wrapper, properties);
+  });
+
+  async function loadTreeSitter() {
+    Theme.load(require("./theme.json")); //"monaco-tree-sitter/themes/tomorrow")); //to use the 'tomorrow' theme
+
+    await Parser.init();
+    const parser = new Parser();
+    const Lang = await Parser.Language.load("./tree-sitter-formula.wasm");
+    parser.setLanguage(Lang);
+
+    const language = new Language(require("./grammar.json"));
+    await language.init(
+      "http://localhost:8080/tree-sitter-formula.wasm",
+      Parser
+    );
+  }
+
+  //function to send text editor values to the server via WebSockets
+  document.getElementById("loadFile").onclick = function () {
+    var msg = {
+      type: "editor",
+      text: window.editor.getValue(),
+    };
+    ws.send(JSON.stringify(msg));
+  };
+
+  //resize the text editor whenever the window is resized
+  addEventListener("resize", (event) => {
+    window.editor.layout();
+  });
+
   //return the html for the text editor
   return (
     <>
-      <Editor
-        height="90vh"
-        defaultLanguage="javascript"
-        defaultValue={formulaCode}
-        onMount={handleEditorDidMount}
-      />
+      <div id="textEditor"></div>
     </>
   );
 }
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,6 +3,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -29,7 +30,9 @@ module.exports = {
     rules: [
       {
         test: /\.wasm$/,
-        use: { loader: "file-loader" },
+        // use: { loader: "file-loader" },
+        loader: "file-loader",
+        type: "javascript/auto",
       },
       {
         test: /\.js$/,
@@ -53,7 +56,12 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        // use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ttf$/,
+        use: ["file-loader"],
       },
     ],
   },
@@ -74,7 +82,11 @@ module.exports = {
       filename: "[name].css",
       chunkFilename: "[id].css",
     }),
+    new MonacoWebpackPlugin(),
   ],
+  // node: {
+  //   fs: "empty", // See https://github.com/tree-sitter/tree-sitter/issues/466
+  // },
 };
 
 // uncomment for webpack.prod.config.js file corresponding to updated package.json file
